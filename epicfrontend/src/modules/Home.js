797f1a3d5e029c61057fb5984412f7c1e1eaece7
Zohaib haider <zebihaider123@gmail.com>
2023-11-20 16:23:22 +0500

    changes

---
+++ epicfrontend/src/modules/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-black font-custom'> hye this is my fyp of website named as epic explorer</h1>
+            <p className='text-orange-600 font-bold font-custom'>This is my Website Broooo</p>
+        </div>
+    )
+}
+
+export default Home
